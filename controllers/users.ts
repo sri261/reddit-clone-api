4e9b084db5caa354ce7af96261f2cd2b7affd9e8
sriharsh <dsriharsh@gmail.com>
2021-03-18 13:10:35 +0530

    login route

--- controllers/users.ts
+++ controllers/users.ts
@@ -19,6 +19,26 @@ module.exports.signup = async (request: any, response: any) => {
 };
 
 module.exports.login = async (request: any, response: any) => {
-  const user = await User.query().findOne({ username: request.body.username });
-  response.send(user);
+  try {
+    const user = await User.query().findOne({
+      username: request.body.username,
+    });
+    if (!user) {
+      response.send("username does not exist");
+    } else {
+      const passwordCheck = await bcrypt.compare(
+        request.body.password,
+        user.password
+      );
+      passwordCheck
+        ? response.json({
+            id: user.id,
+            username: user.username,
+            token: user.token,
+          })
+        : response.send("enter correct password");
+    }
+  } catch (error) {
+    response.send(error);
+  }
 };
